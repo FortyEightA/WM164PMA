38530ccf03c4ff3f220af5a02a78db6da56b2001
FortyEightA <iggyniznik@gmail.com>
2023-12-07 15:22:01 +0000

    Classess fully work.

--- app.py
+++ app.py
@@ -28,29 +28,10 @@ class Data(pd.DataFrame):
         print(self.get_location())
         print(self.head())
 
-#######################################################################
-# Section of code that is used to create graphs from the dataframes.  #
-#######################################################################
-
-# Function that takes a dataframe, x and y axis, title, mode, and file#
-#name and creates a scatter plot image.
-def scatter_plot_to_image(data_frame, x, y, title, mode, file_name):
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=data_frame[x], y=data_frame[y], mode=mode))
-    fig.update_layout(
-        autotypenumbers='convert types',
-        title=title,
-        title_x=0.5)
-    fig.write_image(file_name + ".png")
-
-#######################################################################
-# Section of code that is used to find avg difference between values. #
-#######################################################################
-
 # Decorator to change nan values to 0.
 
 
-def nan_to_zero_decorator(func):
+def float_nan_to_zero_decorator(func):
     def wrapper(x, y):
         if math.isnan(x):
             return func(0, y) if not (math.isnan(y)) else func(0, 0)
@@ -71,15 +52,40 @@ def larger_smaller_decorator(func):
             return func(y, x)
     return wrapper
 
+def split_three_point_time(data):
+    data_time_values = data.iloc[:, 1:3]
+    print(data_time_values)
+
+
+#######################################################################
+# Section of code that is used to create graphs from the dataframes.  #
+#######################################################################
+
+# Function that takes a dataframe, x and y axis, title, mode, and file#
+#name and creates a scatter plot image.
+def scatter_plot_to_image(data_frame, x, y, title, mode, file_name):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data_frame[x], y=data_frame[y], mode=mode))
+    fig.update_layout(
+        autotypenumbers='convert types',
+        title=title,
+        title_x=0.5)
+    fig.write_image(file_name + ".png")
+
+#######################################################################
+# Section of code that is used to find avg difference between values. #
+#######################################################################
+
+
 # Function that takes two dataframes and returns the average difference
 # between individual values in the two dataframes.
 
 
 def avg_differences(first_data_frame, second_data_frame):
-    first_data_frame = first_data_frame[first_data_frame.columns[2]].astype(float)
-    second_data_frame = second_data_frame[second_data_frame.columns[2]].astype(float)
-    jointArr = pd.concat([first_data_frame, second_data_frame], axis=1).to_numpy()
-    diff_return = nan_to_zero_decorator(larger_smaller_decorator(lambda x, y: x - y))
+    first_data_frame = first_data_frame[first_data_frame.columns[2]]
+    second_data_frame = second_data_frame[second_data_frame.columns[2]]
+    jointArr = pd.concat([first_data_frame, second_data_frame], axis=1).astype(float).to_numpy()
+    diff_return = float_nan_to_zero_decorator(larger_smaller_decorator(lambda x, y: x - y))
     differences = [diff_return(x, y) for x, y in jointArr]
     return np.mean(differences)
 
@@ -97,8 +103,8 @@ def main():
     data_data_frame.columns = ['Date', 'Time', 'PM1.0', 'Date', 'Time', 'PM1.0']
     hce_data_frame = Data(data_data_frame.iloc[:, 0:3], name='HCE', location='HCE')
     cnc_data_frame = Data(data_data_frame.iloc[:, 3:6], name='CNC', location='CNC')
-    hce_data_frame.display()
-    cnc_data_frame.display()
+    # hce_data_frame.display()
+    # cnc_data_frame.display()
     # scatter_plot_to_image(
     #     hce_data_frame,
     #     'Time',
@@ -107,6 +113,7 @@ def main():
     #     'markers',
     #     'HCE PM1.0')
     # print(avg_differences(hce_data_frame, cnc_data_frame))
+    split_three_point_time(hce_data_frame)
     tf = time.time() - t1
     print(tf)
 
