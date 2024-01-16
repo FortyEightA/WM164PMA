4694ac9463a05115455dee238d3511f7fa84d01f
FortyEightA <iggyniznik@gmail.com>
2024-01-16 13:31:59 +0000

    Begining work on creating custom solution to read data. Removed Scipy as this will be done manually, and removed pandas as this will be implemented fully. Commented out old Data class, new data class will have full read and manip functionality. Continuing work on this.

--- app.py
+++ app.py
@@ -1,32 +1,41 @@
-import pandas as pd
+import csv
 import math
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
-import scipy.stats as stats
 import kaleido
 import time
 
-class Data(pd.DataFrame):
+# class Data(pd.DataFrame):
+#     def __init__(self, data, name, location):
+#         super().__init__(data)
+#         self._name = name 
+#         self._location = location 
+#
+#     def get_name(self):
+#         return self._name
+#     def set_name(self, name):
+#         self._name = name
+#
+#     def get_location(self):
+#         return self._location
+#     def set_location(self, location):
+#         self._location = location
+#
+#     def display(self):
+#         print(self.get_name())
+#         print(self.get_location())
+#         print(self.head())
+
+class Data():
     def __init__(self, data, name, location):
-        super().__init__(data)
-        self._name = name 
-        self._location = location 
-
-    def get_name(self):
-        return self._name
-    def set_name(self, name):
+        self._data = data
         self._name = name
-
-    def get_location(self):
-        return self._location
-    def set_location(self, location):
         self._location = location
+             
 
-    def display(self):
-        print(self.get_name())
-        print(self.get_location())
-        print(self.head())
+
+
 
 # Decorator to change nan values to 0.
 
