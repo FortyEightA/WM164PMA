a13e9e8d6c924bab1e9e06c145a460d9afd40c5b
FortyEightA <iggyniznik@gmail.com>
2024-01-26 16:01:04 +0000

    Removed Text class. Rewrote Label class to accomodate. Removed Text Class, as it is probably not needed anymore. Rewrote Label class to allow for better text presentation.

--- app.py
+++ app.py
@@ -10,59 +10,31 @@ ctk.FontManager.load_font("fonts/PublicSans-Regular.ttf")
 
 
 class Titles(ctk.CTkFrame):
-    def __init__(self, master, titles):
-        super().__init__(master, fg_color="transparent")
+    def __init__(self, master, titles, fg_color_input="transparent"):
+        super().__init__(master, fg_color=fg_color_input)
 
         self.titles = titles
         self.width = self.master.winfo_screenwidth()
-        self.title_arr = []
+# Array for each column
+        self.title_arr = [[], [], [], [], [], [], [], []]
 
-        for i, title in enumerate(self.titles):
+        for i, column in enumerate(self.titles):
+            for k, each_label_in_row in enumerate(column):
+                self.grid_rowconfigure(k, weight=1)
+                self.grid_columnconfigure(i, weight=1)
+                self.title = ctk.CTkLabel(
+                    self,
+                    text=each_label_in_row,
+                    width=(int(self.width / len(self.titles)) - 20),
+                    font=("PublicSans-Regular", 20),
+                    corner_radius=6)
+                self.title.grid(
+                    column=i,
+                    row=k,
+                    padx=10,
+                    sticky="new")
+                self.title_arr[i].append(self.title)
 
-            self.grid_rowconfigure(0, weight=1)
-            self.grid_columnconfigure(i, weight=1)
-
-            self.title = ctk.CTkLabel(
-                self,
-                text=title,
-                width=(int(self.width / len(self.titles)) - 20),
-                font=("PublicSans-Regular", 20),
-                corner_radius=6)
-
-            self.title.grid(
-                column=i,
-                row=0,
-                padx=10,
-                sticky="new")
-            self.title_arr.append(self.title)
-
-
-class Text(ctk.CTkFrame):
-    def __init__(self, master, texts):
-        super().__init__(master, fg_color="transparent")
-
-        self.texts = texts
-        self.width = self.master.winfo_screenwidth()
-        self.textboxes = []
-
-        for i, text in enumerate(self.texts):
-            self.grid_columnconfigure(i, weight=1)
-
-            self.textbox = ctk.CTkTextbox(
-                self,
-                width=(int(self.width / len(self.texts)) - 20),
-                height=0,
-                font=("PublicSans-Regular", 20),
-                activate_scrollbars=False,
-                corner_radius=6)
-            self.textbox.insert(0.0, text, "center")
-            self.textbox.configure(state="disabled")
-            self.textbox.grid(
-                column=i,
-                row=0,
-                padx=10,
-                sticky="new")
-            self.textboxes.append(self.textbox)
 
 class Images(ctk.CTkFrame):
     def __init__(self, master, path_to_images, height):
@@ -80,8 +52,8 @@ class Images(ctk.CTkFrame):
             self.image = ctk.CTkImage(
                 light_image=Image.open(path_to_images[i]),
                 dark_image=Image.open(path_to_images[i]),
-                size=(int(self.width / len(self.path_to_images))+50, self.height),
-                )
+                size=(int(self.width / len(self.path_to_images)) + 50, self.height),
+            )
             self.image_frame = ctk.CTkLabel(
                 self,
                 image=self.image,
@@ -109,7 +81,7 @@ class App(ctk.CTk):
         self.grid_columnconfigure((0, 1), weight=1)
         self.grid_rowconfigure(4, weight=1)
 
-        self.middle_title = Titles(self, ["AQM Data Analysis"])
+        self.middle_title = Titles(self, [["AQM Data Analysis"]])
         self.middle_title.grid(
             row=0,
             column=0,
@@ -119,7 +91,7 @@ class App(ctk.CTk):
             columnspan=2)
 
         self.objective_title = Titles(
-            self, ["Mean Difference", "3 Point Standard Deviation"])
+            self, [["Mean Difference"], ["3 Point Standard Deviation"]])
         self.objective_title.grid(
             row=1,
             column=0,
@@ -127,17 +99,20 @@ class App(ctk.CTk):
             pady=(10, 0),
             columnspan=2,
             sticky="new")
-        for i in range(2): self.objective_title.title_arr[i].configure(fg_color="transparent")
+        # fg_color_input="#434C5E"
 
-        self.main_text_boxes = Text(
-            self,
-            [
-                "HCE Mean:  \n\nCNC Mean: \n\nDifference Of Mean: ",
-                "\nLocation of Largest Three Point Standard Deviation: \n\nThree point time window: \n\nLargest Standard Deviation: "])
+        self.main_text_boxes = Titles(self,
+                                      [["HCE Mean:",
+                                        "CNC Mean:",
+                                        "Difference Of Mean:"],
+                                       ["Largest Three Point of HCE:",
+                                        "Largest Three Point of CNC:",
+                                        "Largest Standard Deviation:"]],
+                                      fg_color_input="#434C5E")
         self.main_text_boxes.grid(
             row=2,
             column=0,
-            padx=10,
+            padx=20,
             pady=(10, 0),
             columnspan=2,
             sticky="new")
@@ -166,7 +141,6 @@ class App(ctk.CTk):
             pady=(10, 0),
             columnspan=2,
             sticky="new")
-        for i, imag in enumerate(images_to_display): self.image_labels.title_arr[i].configure(fg_color="transparent")
         self.images = Images(self, path_to_images, image_height)
         self.images.grid(
             row=4,
@@ -193,25 +167,25 @@ if __name__ == "__main__":
         lambda x, y: x if x > y else y)(
         hce_std, cnc_std)
 
-    app.main_text_boxes.textboxes[0].configure(state="normal")
-    app.main_text_boxes.textboxes[1].configure(state="normal")
-    app.main_text_boxes.textboxes[0].delete(0.0, tk.END)
-    app.main_text_boxes.textboxes[1].delete(0.0, tk.END)
-    app.main_text_boxes.textboxes[0].insert(0.0, f"HCE Mean: {hce_mean}\n\nCNC Mean: {
-        cnc_mean}\n\nDifference Of Mean: {difference_of_mean}")
-    app.main_text_boxes.textboxes[1].insert(
-        0.0,
-        f"Largest Three Point of HCE: {hce_std}\n\nLargest Three Point of CNC: {cnc_std}\n\nLargest Standard Deviation: {largest_std}")
+    app.main_text_boxes.title_arr[0][0].configure(text=f"HCE Mean: {hce_mean}")
+    app.main_text_boxes.title_arr[0][1].configure(text=f"CNC Mean: {cnc_mean}")
+    app.main_text_boxes.title_arr[0][2].configure(
+        text=f"Difference Of Mean: {difference_of_mean}")
+    app.main_text_boxes.title_arr[1][0].configure(
+        text=f"Largest Three Point of HCE: {hce_std}")
+    app.main_text_boxes.title_arr[1][1].configure(
+        text=f"Largest Three Point of CNC: {cnc_std}")
+    app.main_text_boxes.title_arr[1][2].configure(
+        text=f"Largest Standard Deviation: {largest_std}")
 
-    app.main_text_boxes.textboxes[0].configure(state="disabled")
-    app.main_text_boxes.textboxes[1].configure(state="disabled")
-    
     hce_data_frame.create_graph()
     cnc_data_frame.create_graph()
-    
-    images_to_display = ["HCE Scatter Graph", "HCE Box Plot"]
-    path_to_images = ["graphs/HCE/HCE Scatter Graph.png","graphs/HCE/HCE Box Plot.png"]
-    
+
+    images_to_display = [["HCE Scatter Graph"], ["HCE Box Plot"]]
+    path_to_images = [
+        "graphs/HCE/HCE Scatter Graph.png",
+        "graphs/HCE/HCE Box Plot.png"]
+
     graph_height = 900
     app.create_images(images_to_display, path_to_images, graph_height)
     app.mainloop()
